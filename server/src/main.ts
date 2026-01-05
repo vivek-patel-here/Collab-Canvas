1798292616560964c7330938676e09e70b075f12
Vivek Patel <vivek.patel.1057@gmail.com>
2026-01-05 12:50:14 +0530

    deployment configs

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin:"http://localhost:3000",
-    origin:"https://collab-canvas-lyart.vercel.app/",
+    origin:"https://collab-canvas-lyart.vercel.app",
     methods:["GET","POST","PATCH","PUT","DELETE"],
     credentials:true
   })
